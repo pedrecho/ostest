3702a05c1a93b85553d70ad8735bdd183f3316d3
pedrecho <pedrecho.flash@gmail.com>
2022-09-28 15:26:14 +0300

    v0.2 add Directory Singleton class

--- main.py
+++ main.py
@@ -1,12 +1,63 @@
 import os
+import sys
 from dotenv import dotenv_values
 
 
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class Directory(metaclass=Singleton):
+    def __init__(self):
+        config = dotenv_values(".env")
+        if not config:
+            print("Не найден файл конфигурации")
+            sys.exit(1)
+        self._root = config["ROOT"]
+        self._path = []
+
+    @property
+    def path(self):
+        return self._path
+
+    @property
+    def root(self):
+        return self._root
+
+    def make_path(self, root=False):
+        s = '~'
+        if root:
+            s = self.root
+        return s + '\\' + '\\'.join(self.path)
+
+
+def os_listdir(cmd, directory):
+    print(os.listdir(directory.make_path(True)))
+    return
+
+# def os_cd(cmd, directory):
+
+
+
 def main():
-    config = dotenv_values(".env")
-    if not config:
-        print("Не найден файл конфигурации")
-        return
+    directory = Directory()
+    commands = {
+        "ls": os_listdir,
+        # "cd": os_cd,
+    }
+    os.chdir(directory.root)
+    while True:
+        print(directory.make_path(), end=' ')
+        cmd = input().split(' ')
+        try:
+            commands[cmd[0]](cmd[1:], directory)
+        except KeyError:
+            print('Неизвестная команда "' + cmd[0] + '"')
 
 
 
